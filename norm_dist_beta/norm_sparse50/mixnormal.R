cca15877290226c507a328c130898e98398cab7d
J. Zhou <summer072591@users.noreply.github.com>
2022-06-26 18:24:47 +0200

    Delete norm_dist_beta directory

--- norm_dist_beta/norm_sparse50/mixnormal.R
+++
@@ -1,9 +0,0 @@
-## mixture normal 
-mixnormal <- function(n, mean1, mean2, sd1, sd2, mix_prob){
-  sample_prob <- runif(n)
-  
-  samples <- sapply(sample_prob, function(x){
-                                 if(x <= mix_prob) return(rnorm(1, mean1, sd1))
-                                 else return(rnorm(1, mean2, sd1))})
-  return(samples)
-}
